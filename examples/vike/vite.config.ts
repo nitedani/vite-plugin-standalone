893c6c98bd560059a6e184f600bd27085d1e7bdc
Dániel Horváth <horvath.daniel@lexunit.hu>
2023-10-31 01:08:45 +0100

    format

--- examples/vike/vite.config.ts
+++ examples/vike/vite.config.ts
@@ -1,28 +1,28 @@
-import react from '@vitejs/plugin-react'
-import ssr from 'vike/plugin'
-import { UserConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import ssr from "vike/plugin";
+import { UserConfig } from "vite";
 import { vavite } from "vavite";
-import {standalone} from "vite-plugin-standalone"
+import { standalone } from "vite-plugin-standalone";
 const config: UserConfig = {
-	buildSteps: [
-		{ name: "client" },
-		{
-			name: "server",
-			config: {
-				build: { ssr: true },
-			},
-		},
-	],
+  buildSteps: [
+    { name: "client" },
+    {
+      name: "server",
+      config: {
+        build: { ssr: true },
+      },
+    },
+  ],
 
-	plugins: [
-		vavite({
-			serverEntry: "/server/index.ts",
-			serveClientAssetsInDev: true,
-		}),
-		react(),
-		ssr({ disableAutoFullBuild: true }),
-    standalone()
-	],
-}
+  plugins: [
+    vavite({
+      serverEntry: "/server/index.ts",
+      serveClientAssetsInDev: true,
+    }),
+    react(),
+    ssr({ disableAutoFullBuild: true }),
+    standalone(),
+  ],
+};
 
-export default config
+export default config;
