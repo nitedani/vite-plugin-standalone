6b70f510933678c3115e29a2681794c751f7c4e7
Dániel Horváth <horvath.daniel@lexunit.hu>
2023-12-09 13:09:56 +0100

    use @vercel/nft

--- packages/vite-plugin-standalone/src/index.ts
+++ packages/vite-plugin-standalone/src/index.ts
@@ -1,30 +1,39 @@
-import { existsSync, readFileSync } from 'fs';
-import { writeFile, mkdir } from 'fs/promises';
+import { Sema } from 'async-sema';
+import { existsSync } from 'fs';
+import fs from 'fs/promises';
 import { builtinModules } from 'module';
-import { join, dirname } from 'path';
-import { Plugin } from 'vite';
+import path from 'path';
+import { Plugin, searchForWorkspaceRoot } from 'vite';
 
 export const standalone = (): Plugin => {
   let root = '';
   let outDir = '';
 
-  const external = [...builtinModules, ...builtinModules.map(m => `node:${m}`)];
+  // Need to list native dependencies here
+  const external = [
+    'sharp',
+    '@generated/prisma',
+    '@prisma/client',
+    '@node-rs/argon2',
+    ...builtinModules,
+    ...builtinModules.map(m => `node:${m}`),
+  ];
   const noExternalRegex = new RegExp(`^(?!(${external.join('|')})$)`);
 
   return {
     name: 'vite-plugin-standalone',
     apply(config, env) {
-      return !!env.isSsrBuild
+      return !!env.isSsrBuild;
     },
     enforce: 'post',
-    // config(config, env) {
-    //   return {
-    //     ssr: {
-    //       external: ['sharp', '@node-rs/argon2',  ...external],
-    //       // noExternal: noExternalRegex,
-    //     },
-    //   };
-    // },
+    config(config, env) {
+      return {
+        ssr: {
+          external,
+          noExternal: [noExternalRegex],
+        },
+      };
+    },
 
     configResolved(config) {
       root = config.root;
@@ -32,39 +41,19 @@ export const standalone = (): Plugin => {
     },
 
     async closeBundle() {
-      const outDirAbs = join(root, outDir);
+      const outDirAbs = path.join(root, outDir);
+      const workspaceRoot = searchForWorkspaceRoot(root);
+      const relativeRoot = path.relative(workspaceRoot, root);
+
       const entryNames = ['index.js', 'main.js', 'index.mjs', 'main.mjs'];
       const entry = entryNames
-        .map(e => join(outDirAbs, e))
+        .map(e => path.join(outDirAbs, e))
         .find(e => existsSync(e));
       if (!entry) {
         return;
       }
-      const { default: ncc } = await import('@vercel/ncc');
 
-      const { code, map, assets } = await ncc(entry, {
-        // provide a custom cache path or disable caching
-        // cache: './custom/cache/path' | false,
-        // externals to leave as requires of the build
-        // externals: ['externalpackage'],
-        externals: external,
-        // directory outside of which never to emit assets
-        // filterAssetBase: join(cwd(), "..", "..", "..", ".."),
-        // minify: false, // default
-        // sourceMap: false, // default
-        assetBuilds: true, // default
-        // sourceMapBasePrefix: '../', // default treats sources as output-relative
-        // when outputting a sourcemap, automatically include
-        // source-map-support in the output file (increases output by 32kB).
-        // sourceMapRegister: true, // default
-        // watch: false, // default
-        // license: '', // default does not generate a license file
-        // target: 'es2015', // default
-        // v8cache: false, // default
-        // quiet: false, // default
-        // debugLog: false, // default
-      });
-
+      //TODO: do we need this?
       const banner = `
 import { dirname as dirname2 } from 'path';
 import { fileURLToPath as fileURLToPath2 } from 'url';
@@ -73,77 +62,71 @@ var __filename = fileURLToPath2(import.meta.url);
 var __dirname = dirname2(__filename);
 var require = createRequire2(import.meta.url);
       `;
-      await writeFile(entry, banner + code);
 
-      for (const [key, value] of Object.entries(assets)) {
-        let fname = key;
-        if (fname && value && typeof value === 'object' && 'source' in value) {
-          if (fname === 'package.json') {
-            continue;
-          }
+      const { nodeFileTrace } = await import('@vercel/nft');
+      const result = await nodeFileTrace([entry], {
+        base: workspaceRoot,
+        processCwd: workspaceRoot,
+      });
 
-          let source = value.source as Buffer | string;
-          if (fname.endsWith(".mjs") || fname.endsWith(".js")) {
-              source = banner + source.toString("utf-8")
-          }
-
-          if (fname.endsWith('schema.prisma')) {
-            const last = fname.split('/').pop();
-            if (last) {
-              fname = last;
-            }
-          }
-
-          if (fname.includes('libquery')) {
-            const last = fname.split('/').pop();
-            if (last) {
-              fname = last;
-            }
-          }
-
-          const loc = fname.indexOf('node_modules');
-          if (loc) {
-            fname = fname.substring(loc);
-          }
-
-          try {
-            await mkdir(dirname(join(outDirAbs, fname)), { recursive: true });
-          } catch (error) {}
-          await writeFile(join(outDirAbs, fname), source as string);
+      const tracedDeps = new Set<string>();
+      for (const file of result.fileList) {
+        if (result.reasons.get(file)?.type.includes('initial')) {
+          continue;
         }
+        tracedDeps.add(file.replace(/\\/g, '/'));
       }
+      const files = [...tracedDeps];
 
-      // TODO: @vercel/nft doesn't detect prisma dependencies in the context of a pnpm monorepo
-      //   const { nodeFileTrace } = await import('@vercel/nft');
-      //   const { fileList } = await nodeFileTrace([entry]);
-      //   const toCopy = [...fileList].filter(f => !f.startsWith(outDir));
-      //   console.log(fileList);
-      // for (const file of toCopy) {
-      //     const dest = join(outDirAbs, file);
-      //     if (statSync(file).isDirectory()) {
-      //         continue;
-      //     }
-      //     // const dir = isFile ? dirname(dest) : dest;
-      //     try {
-      //         await mkdir(dirname(dest), { recursive: true });
-      //     }
-      //     catch (error) { }
-      //     await copyFile(file, dest);
-      // }
+      const copySema = new Sema(10, { capacity: files.length });
+      const copiedFiles = new Set<string>();
 
-      const packageJson = JSON.parse(
-        readFileSync(join(root, 'package.json'), 'utf-8'),
-      );
-      const packageJsonNew = {
-        name: packageJson.name,
-        version: packageJson.version,
-        type: 'module',
-        private: true,
-        scripts: packageJson.scripts,
-      };
-      await writeFile(
-        join(outDirAbs, 'package.json'),
-        JSON.stringify(packageJsonNew, null, 2),
+      await Promise.all(
+        files.map(async relativeFile => {
+          await copySema.acquire();
+
+          const tracedFilePath = path.join(workspaceRoot, relativeFile);
+
+          ///////////////////////////////////
+          // This is to support pnpm monorepo
+          let segments = 0;
+          if (relativeFile.startsWith(`${relativeRoot}/`)) {
+            segments = `${relativeRoot}/`.match(/\//g)?.length ?? 0;
+            relativeFile = relativeFile.replace(`${relativeRoot}/`, '');
+          }
+          ///////////////////////////////////
+
+          const fileOutputPath = path.join(outDirAbs, relativeFile);
+
+          if (!copiedFiles.has(fileOutputPath)) {
+            copiedFiles.add(fileOutputPath);
+
+            await fs.mkdir(path.dirname(fileOutputPath), { recursive: true });
+            let symlink = await fs.readlink(tracedFilePath).catch(() => null);
+
+            if (symlink) {
+              ///////////////////////////////////
+              // This is to support pnpm monorepo
+              if (segments) {
+                const idx = symlink.split('/', segments).join('/').length + 1;
+                symlink = symlink.substring(idx);
+              }
+              ///////////////////////////////////
+
+              try {
+                await fs.symlink(symlink, fileOutputPath);
+              } catch (e: any) {
+                if (e.code !== 'EEXIST') {
+                  throw e;
+                }
+              }
+            } else {
+              await fs.copyFile(tracedFilePath, fileOutputPath);
+            }
+          }
+
+          copySema.release();
+        }),
       );
     },
   };
