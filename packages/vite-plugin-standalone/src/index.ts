05821703cd69ee5cb6a77785d0e6f317ad813f80
Dániel Horváth <horvath.daniel@lexunit.hu>
2024-01-31 04:48:00 +0100

    support nestjs and lazy requires

--- packages/vite-plugin-standalone/src/index.ts
+++ packages/vite-plugin-standalone/src/index.ts
@@ -1,72 +1,173 @@
 import { Sema } from 'async-sema';
-import { existsSync } from 'fs';
+import esbuild from 'esbuild';
 import fs from 'fs/promises';
-import { builtinModules } from 'module';
 import path from 'path';
 import { Plugin, searchForWorkspaceRoot } from 'vite';
 
-export const standalone = (): Plugin => {
+function assert(condition: unknown, debugInfo?: unknown): asserts condition {
+  if (condition) return;
+
+  const debugStr = (() => {
+    if (!debugInfo) {
+      return null;
+    }
+    const debugInfoSerialized =
+      typeof debugInfo === 'string' ? debugInfo : JSON.stringify(debugInfo);
+    return `Debug info (for vite-plugin-standalone maintainers; you can ignore this): ${debugInfoSerialized}`;
+  })();
+
+  let errMsg = [
+    `You stumbled upon a bug in vite-plugin-standalone's source code.`,
+    `Go to https://github.com/nitedani/vite-plugin-standalone and copy-paste this error.`,
+    debugStr,
+  ]
+    .filter(Boolean)
+    .join(' ');
+
+  throw new Error(errMsg);
+}
+
+type StandaloneOptions = {
+  native?: string[];
+};
+
+export const standalone = (options: StandaloneOptions = {}): Plugin => {
   let root = '';
   let outDir = '';
-
-  // Need to list native dependencies here
-  const external = [
+  let distDir = '';
+  let outDirAbs = '';
+  let serverIndexFilePath: string;
+  let rollupResolve: any;
+  // Native dependencies always need to be esbuild external
+  let native: string[] = [
     'sharp',
     '@generated/prisma',
     '@prisma/client',
     '@node-rs/argon2',
-    ...builtinModules,
-    ...builtinModules.map(m => `node:${m}`),
+    ...(options.native ?? []),
   ];
-  const noExternalRegex = new RegExp(`^(?!(${external.join('|')})$)`);
+
+  // https://github.com/nestjs/nest-cli/blob/edbd64d1eb186c49c28b7594e5d8269a5b125385/lib/compiler/defaults/webpack-defaults.ts#L69
+  const lazyNpmImports = [
+    '@nestjs/microservices',
+    '@nestjs/websockets',
+    'cache-manager',
+    'class-validator',
+    'class-transformer',
+  ];
 
   return {
     name: 'vite-plugin-standalone',
-    apply(config, env) {
-      return !!env.isSsrBuild;
+    apply(_, env) {
+      //@ts-expect-error Vite 5 || Vite 4
+      return !!(env.isSsrBuild || env.ssrBuild);
     },
     enforce: 'post',
     config(config, env) {
       return {
         ssr: {
-          external,
-          noExternal: [noExternalRegex],
+          noExternal: ['@brillout/picocolors'],
+          external: native,
+        },
+        optimizeDeps: {
+          exclude: native,
         },
       };
     },
-
-    configResolved(config) {
-      root = config.root;
-      outDir = config.build.outDir;
+    buildStart() {
+      rollupResolve = this.resolve.bind(this);
     },
-
+    async configResolved(config) {
+      root = toPosixPath(config.root);
+      outDir = toPosixPath(config.build.outDir);
+      distDir = outDir.split('/')[0]!;
+      outDirAbs = path.posix.join(root, outDir);
+    },
+    writeBundle(_, bundle) {
+      const serverIndex = findRollupBundleEntry('index', bundle);
+      assert(serverIndex);
+      const serverIndexFileName = serverIndex.fileName;
+      serverIndexFilePath = path.posix.join(outDirAbs, serverIndexFileName);
+    },
     async closeBundle() {
-      const outDirAbs = path.join(root, outDir);
-      const workspaceRoot = searchForWorkspaceRoot(root);
-      const relativeRoot = path.relative(workspaceRoot, root);
+      const res = await esbuild.build({
+        platform: 'node',
+        format: 'esm',
+        bundle: true,
+        external: native,
+        entryPoints: { index: serverIndexFilePath },
+        outfile: serverIndexFilePath,
+        allowOverwrite: true,
+        metafile: true,
+        banner: {
+          js: [
+            "import { dirname as dirname987 } from 'path';",
+            "import { fileURLToPath as fileURLToPath987 } from 'url';",
+            "import { createRequire as createRequire987 } from 'module';",
+            'var require = createRequire987(import.meta.url);',
+            'var __filename = fileURLToPath987(import.meta.url);',
+            'var __dirname = dirname987(__filename);',
+          ].join('\n'),
+        },
+        plugins: [
+          {
+            name: 'standalone-ignore',
+            setup(build) {
+              build.onResolve({ filter: /.*/, namespace: 'ignore' }, args => ({
+                path: args.path,
+                namespace: 'ignore',
+              }));
 
-      const entryNames = ['index.js', 'main.js', 'index.mjs', 'main.mjs'];
-      const entry = entryNames
-        .map(e => path.join(outDirAbs, e))
-        .find(e => existsSync(e));
-      if (!entry) {
-        return;
+              build.onResolve(
+                { filter: new RegExp(`^(${lazyNpmImports.join('|')})`) },
+                async args => {
+                  const resolved = await rollupResolve(args.path);
+                  if (!resolved) {
+                    return {
+                      path: args.path,
+                      namespace: 'ignore',
+                    };
+                  }
+                },
+              );
+
+              build.onLoad({ filter: /.*/, namespace: 'ignore' }, () => ({
+                contents: '',
+              }));
+            },
+          },
+        ],
+      });
+
+      // The inputs of the bundled files are safe to remove
+      const filesToRemove = Object.keys(res.metafile.inputs).filter(
+        relativeFile =>
+          !serverIndexFilePath.endsWith(relativeFile) &&
+          relativeFile.startsWith(distDir),
+      );
+      for (const relativeFile of filesToRemove) {
+        await fs.rm(path.posix.join(root, relativeFile));
       }
 
-      //TODO: do we need this?
-      const banner = `
-import { dirname as dirname2 } from 'path';
-import { fileURLToPath as fileURLToPath2 } from 'url';
-import { createRequire as createRequire2 } from 'module';
-var __filename = fileURLToPath2(import.meta.url);
-var __dirname = dirname2(__filename);
-var require = createRequire2(import.meta.url);
-      `;
+      // Remove leftover empty dirs
+      const relativeDirs = unique(
+        filesToRemove.map(file => path.dirname(file)),
+      );
+      for (const relativeDir of relativeDirs) {
+        const absDir = path.posix.join(root, relativeDir);
+        const files = await fs.readdir(absDir);
+        if (!files.length) {
+          await fs.rm(absDir, { recursive: true });
+        }
+      }
+
+      const base = toPosixPath(searchForWorkspaceRoot(root));
+      const relativeRoot = path.posix.relative(base, root);
+      const relativeDistDir = path.posix.relative(base, distDir);
 
       const { nodeFileTrace } = await import('@vercel/nft');
-      const result = await nodeFileTrace([entry], {
-        base: workspaceRoot,
-        processCwd: workspaceRoot,
+      const result = await nodeFileTrace([serverIndexFilePath], {
+        base,
       });
 
       const tracedDeps = new Set<string>();
@@ -74,55 +175,51 @@ var require = createRequire2(import.meta.url);
         if (result.reasons.get(file)?.type.includes('initial')) {
           continue;
         }
-        tracedDeps.add(file.replace(/\\/g, '/'));
+        tracedDeps.add(toPosixPath(file));
       }
-      const files = [...tracedDeps];
 
+      const files = [...tracedDeps].filter(
+        path => !path.startsWith(relativeDistDir) && !path.startsWith('usr/'),
+      );
+
+      // We are done, no native dependencies need to be copied
+      if (!files.length) {
+        return;
+      }
+
+      if (result.warnings.size && isYarnPnP()) {
+        throw new Error(
+          'Standalone build is not supported when using Yarn PnP and native dependencies.',
+        );
+      }
+
+      const commonAncestor = findCommonAncestor(files);
       const copySema = new Sema(10, { capacity: files.length });
-      const copiedFiles = new Set<string>();
 
+      const copiedFiles = new Set<string>();
       await Promise.all(
         files.map(async relativeFile => {
           await copySema.acquire();
+          const tracedFilePath = path.posix.join(base, relativeFile);
+          const isNodeModules = relativeFile.includes('node_modules');
 
-          const tracedFilePath = path.join(workspaceRoot, relativeFile);
+          relativeFile = relativeFile
+            .replace(relativeRoot, '')
+            .replace(commonAncestor, '');
+          const relativeFileHoisted = `node_modules${relativeFile
+            .split('node_modules')
+            .pop()}`;
+          const fileOutputPath = path.posix.join(
+            outDirAbs,
+            isNodeModules ? relativeFileHoisted : relativeFile,
+          );
+          const isDir = (await fs.stat(tracedFilePath)).isDirectory();
 
-          ///////////////////////////////////
-          // This is to support pnpm monorepo
-          let segments = 0;
-          if (relativeFile.startsWith(`${relativeRoot}/`)) {
-            segments = `${relativeRoot}/`.match(/\//g)?.length ?? 0;
-            relativeFile = relativeFile.replace(`${relativeRoot}/`, '');
-          }
-          ///////////////////////////////////
-
-          const fileOutputPath = path.join(outDirAbs, relativeFile);
-
-          if (!copiedFiles.has(fileOutputPath)) {
+          if (!isDir && !copiedFiles.has(fileOutputPath)) {
             copiedFiles.add(fileOutputPath);
-
-            await fs.mkdir(path.dirname(fileOutputPath), { recursive: true });
-            let symlink = await fs.readlink(tracedFilePath).catch(() => null);
-
-            if (symlink) {
-              ///////////////////////////////////
-              // This is to support pnpm monorepo
-              if (segments) {
-                const idx = symlink.split('/', segments).join('/').length + 1;
-                symlink = symlink.substring(idx);
-              }
-              ///////////////////////////////////
-
-              try {
-                await fs.symlink(symlink, fileOutputPath);
-              } catch (e: any) {
-                if (e.code !== 'EEXIST') {
-                  throw e;
-                }
-              }
-            } else {
-              await fs.copyFile(tracedFilePath, fileOutputPath);
-            }
+            await fs.cp(await fs.realpath(tracedFilePath), fileOutputPath, {
+              recursive: true,
+            });
           }
 
           copySema.release();
@@ -131,3 +228,62 @@ var require = createRequire2(import.meta.url);
     },
   };
 };
+function toPosixPath(path: string): string {
+  const pathPosix = path.split('\\').join('/');
+  return pathPosix;
+}
+function findRollupBundleEntry<
+  OutputBundle extends Record<string, { name: string | undefined }>,
+>(entryName: string, bundle: OutputBundle): OutputBundle[string] | null {
+  for (const key in bundle) {
+    if (key.endsWith('.map')) continue; // https://github.com/brillout/vite-plugin-ssr/issues/612
+    const entry = bundle[key]!;
+    if (entry.name === entryName) return entry;
+  }
+  return null;
+}
+function unique<T>(arr: T[]): T[] {
+  return Array.from(new Set(arr));
+}
+function findCommonAncestor(paths: string[]) {
+  // There is no common anchestor of 0 or 1 path
+  if (paths.length <= 1) {
+    return '';
+  }
+
+  // Split each path into its components
+  const pathsComponents = paths.map(path => path.split('/'));
+
+  let commonAncestor = '';
+  let index = 0;
+
+  assert(pathsComponents.length, { paths });
+
+  // While there is a common component at the current index
+  while (
+    pathsComponents.every(
+      components => components[index] === pathsComponents[0]![index],
+    )
+  ) {
+    // Add the common component to the common ancestor path
+    commonAncestor += pathsComponents[0]![index] + '/';
+    index++;
+  }
+
+  // If no common ancestor was found, return an empty string
+  if (commonAncestor === '') {
+    return '';
+  }
+
+  // Otherwise, return the common ancestor path, removing the trailing slash
+  return commonAncestor.slice(0, -1);
+}
+
+function isYarnPnP(): boolean {
+  try {
+    require('pnpapi');
+    return true;
+  } catch {
+    return false;
+  }
+}
